4f2d3420e41caae3c55f546b5b584fa0c6059a4b
luii0415 <becky45210193@gmail.com>
2023-05-12 13:43:50 +0900

    luii_react_folder

---
+++ luii_project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <div className="top"> top section</div>
+      <div className="middle"> middle section</div>
+      <div className="bottom">bottom section</div>
+    </div>
+  );
+}
+
+export default App;
