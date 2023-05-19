85b2a5158dc3f9d38427f5edbc7cd55c34d9ac78
HakjinKim <9809king@naver.com>
2023-05-19 13:46:36 +0900

    정적 지도 추가

--- project_hakjin/src/App.js
+++ project_hakjin/src/App.js
@@ -21,18 +21,18 @@ function App() {
 function Kakao(){
 
   useEffect(() => {
-    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    const options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3 //지도의 레벨(확대, 축소 정도)
+    const staticMapContainer  = document.getElementById('staticMap'); //지도를 담을 영역의 DOM 레퍼런스
+    const staticMapOption = { //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(35.95, 128.25), //지도의 중심좌표.
+      level: 14 //지도의 레벨(확대, 축소 정도)
     };
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    const staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption); //지도 생성 및 객체 리턴
   }, [])
   
   return (
-    <div id='map' style={{
-      width : '500px',
-      height : '500px'
+    <div id='staticMap' style={{
+      width : '300px',
+      height : '450px'
     }}></div>
   )
 
