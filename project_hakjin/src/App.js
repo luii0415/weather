f771b317d435fbe675cc7ee8c942c4c20758a36a
HakjinKim <9809king@naver.com>
2023-05-24 14:44:27 +0900

    지도에 마커 추가

--- project_hakjin/src/App.js
+++ project_hakjin/src/App.js
@@ -1,42 +1,71 @@
 import './App.css';
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
-const {kakao} = window;
+const { kakao } = window;
 
 function App() {
 
   useEffect(() => {
-    const staticMapContainer  = document.getElementById('staticMap'); //지도를 담을 영역의 DOM 레퍼런스
-    const staticMapOption = { //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(35.95, 128.25), //지도의 중심좌표.
-      level: 14 //지도의 레벨(확대, 축소 정도)
+    const staticMapContainer = document.getElementById('staticMap'); // 지도를 담을 영역의 DOM 레퍼런스
+    const mapOption = { // 지도를 생성할 때 필요한 옵션
+      center: new kakao.maps.LatLng(35.95, 128.25), // 지도의 중심좌표
+      level: 14 // 지도의 레벨(확대, 축소 정도)
     };
-    const staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption); //지도 생성 및 객체 리턴
-    kakao.maps.event.addListener(staticMap, 'click', function(mouseEvent) {        
-    
-      // 클릭한 위도, 경도 정보를 가져옵니다 
-      var latlng = mouseEvent.latLng; 
-      var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
-      message += '경도는 ' + latlng.getLng() + ' 입니다';
-      
-      var resultDiv = document.getElementById('clickLatlng'); 
-      console.log(resultDiv, message);
-    });
+    const map = new kakao.maps.Map(staticMapContainer, mapOption); // 지도 생성 및 객체 리턴
 
+    // 마커를 표시할 위치와 title 객체 배열입니다 
+    var positions = [
+      {
+        title: '카카오',
+        latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+      },
+      {
+        title: '생태연못',
+        latlng: new kakao.maps.LatLng(33.450936, 126.569477)
+      },
+      {
+        title: '텃밭',
+        latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+      },
+      {
+        title: '근린공원',
+        latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+      }
+    ];
+
+    // 마커 이미지의 이미지 주소입니다
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+    for (var i = 0; i < positions.length; i++) {
+
+      // 마커 이미지의 이미지 크기입니다
+      var imageSize = new kakao.maps.Size(24, 35);
+
+      // 마커 이미지를 생성합니다    
+      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+      // 마커를 생성합니다
+      var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image: markerImage // 마커 이미지 
+      });
+    }
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src='img/CodeBreakers_logo.png'/>
+        <img src='img/CodeBreakers_logo.png' alt="logo" />
         <p>날씨 정보 제공 웹사이트</p>
         <div id='staticMap' style={{
-          width : '400px',
-          height : '450px'
+          width: '400px',
+          height: '450px'
         }}></div>
       </header>
     </div>
   );
 }
 
-export {App};
+export { App };
