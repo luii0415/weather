31c66b630fa217679d2e18ae3c565d95b8563076
luii0415 <becky45210193@gmail.com>
2023-06-07 19:27:47 +0900

    Weather.js에 주석을 추가

--- weather-information/src/Weather.js
+++ weather-information/src/Weather.js
@@ -7,14 +7,15 @@ function Weather(props) {
   const date = current.getDate().toString().padStart(2, "0"); //padStart()메소드 사용 : 일이 2자리가 아니면 앞에 0을 추가
   const formattedDate = `${year}${month}${date}`; //api에 전달되는 날짜
 
-  const [data, setData] = React.useState(null);
-  var { value1, value2, value3 } = props.values;
+  const [data, setData] = React.useState(null); //api 데이터 저장
+  var { value1, value2, value3 } = props.values; //Map에서 전달한 prop 저장
   const val_x = `${value1}`;
   const val_y = `${value2}`;
   const val_locate = `${value3}`;
-  console.log(formattedDate);
 
+  //api 호출 함수
   const fetchData = async () => {
+    // 이하 api 호출 코드
     const url =
       "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
     const serviceKey =
@@ -33,7 +34,7 @@ function Weather(props) {
       `&base_time=${encodeURIComponent(baseTime)}` +
       `&nx=${encodeURIComponent(nx)}` +
       `&ny=${encodeURIComponent(ny)}`;
-
+    //try 사용 : error 방지
     try {
       const response = await fetch(url + queryParams);
       if (response.ok) {
@@ -41,13 +42,15 @@ function Weather(props) {
         setData(jsonData);
         console.log(jsonData); // 출력
       } else {
+        //api 호출 실패시
         console.error("API 호출 실패");
       }
+      //try 실패시 동작
     } catch (error) {
       console.error(error);
     }
   };
-
+  //prop 으로 전달받은 값이 변동될 때마다 api 재호출
   React.useEffect(() => {
     fetchData();
   }, [props.values]);
@@ -65,9 +68,11 @@ function Weather(props) {
       </div>
       <p className="Weather_space"></p>
       <div className="Weather_data">
+        {/* api 데이터 출력부 */}
         {data && data.response && data.response.body && (
           <div>
             <ul>
+              {/* i는 data.response.body.items.item 배열의 요소*/}
               {data.response.body.items.item.map((item, index) => {
                 const popValue = data.response.body.items.item.find(
                   (i) => i.category === "POP"
@@ -77,6 +82,9 @@ function Weather(props) {
                   popValue === "0"
                 ) {
                   return null;
+                }
+                {
+                  /* 특정 정보는 보이지 않도록 if문으로 작성*/
                 }
                 if (
                   item.category !== "WAV" &&
@@ -108,6 +116,7 @@ function Weather(props) {
                         }
                       })()}
                       :{" "}
+                      {/* 일부 요소의 값은 일정한 정수 값을 가지고, 그 정수값에 따른 정보는 case로 구별하여 알 수 있게 한다.*/}
                       {(() => {
                         if (item.category === "SKY") {
                           switch (item.fcstValue) {
@@ -137,6 +146,7 @@ function Weather(props) {
                           return item.fcstValue;
                         }
                       })()}
+                      {/* 일반적인 날씨 정보의 값이다. 단위를 붙여 출력되도록 한다.*/}
                       {(() => {
                         if (!isNaN(item.fcstValue)) {
                           switch (item.category) {
