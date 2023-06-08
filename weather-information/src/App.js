6ca96c23829575a2e8daf15ea9ce9b44fc693b7d
luii0415 <becky45210193@gmail.com>
2023-06-08 15:43:36 +0900

    bottom 출처 표기

--- weather-information/src/App.js
+++ weather-information/src/App.js
@@ -21,7 +21,19 @@ function App() {
         <div className="space"></div>
         <Weather values={values} />
       </div>
-      <div className="bottom">bottom section</div>
+      <div className="bottom">
+        <img className="inline" src="img/img_opentype01.png" />
+        <div className="inline">
+          <p className="p_top">
+            본 저작물은 '기상청'에서 '2021'년작성하여 공공누리 제 1유형으로
+            개방한 '기상청_단기예보 ((구)_동네예보) 조회서비스'를 이용하였으며,
+          </p>
+          <p className="p_bottom">
+            해당 저작물은 <a href="https://www.data.go.kr/">'공공데이터포털'</a>
+            , 'https://www.data.go.kr/' 에서 무료로 다운받으실 수 있습니다.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
